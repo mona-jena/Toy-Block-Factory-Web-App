da2de12b96585b1854f6bebfda9a90b3f4ae54f2
Mona <mona.jena@myob.com>
2021-04-12 21:55:43 +1200

    reformatted

--- ToyBlockFactoryWebApp/Router.cs
+++ ToyBlockFactoryWebApp/Router.cs
@@ -10,7 +10,8 @@ namespace ToyBlockFactoryWebApp
 
         public Router(OrderController orderController)
         {
-            _handlers = new IRequestHandler[] {new HealthCheckHandler(), 
+            _handlers = new IRequestHandler[] {
+                new HealthCheckHandler(), 
                 new OrderCreateHandler(orderController), 
                 new OrderAddBlocksHandler(orderController), 
                 new SingleOrderGetHandler(orderController),
@@ -26,7 +27,7 @@ namespace ToyBlockFactoryWebApp
             var request = context.Request;
             var handler = _handlers.FirstOrDefault(h => h.ShouldHandle(request.Url?.AbsolutePath, request.HttpMethod));
             if (handler != null)
-            {
+            {   
                 var responseHandler = handler.Handle(request);
                 responseHandler.Respond(context.Response);
             }
