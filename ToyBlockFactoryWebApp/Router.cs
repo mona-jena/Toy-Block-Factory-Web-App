d448dfb1f65d22b64882582de27b9a1f5a3d285b
Mona <mona.jena@myob.com>
2021-04-12 16:55:01 +1200

    deleted unecessary code

--- ToyBlockFactoryWebApp/Router.cs
+++ ToyBlockFactoryWebApp/Router.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace ToyBlockFactoryWebApp
 {
@@ -32,92 +28,9 @@ namespace ToyBlockFactoryWebApp
             {
                 var responseHandler = handler.Handle(request);
                 responseHandler.Respond(context.Response);
-                return;
             }
-            /*switch (request.Url?.AbsolutePath)
-            {
-                case "/order" when request.HttpMethod == "POST":
-                    var orderId = _orderController.Post(requestBody);
-                    if (orderId == null) SendResponse(context.Response, HttpStatusCode.BadRequest);
-                    else SendResponse(context.Response, HttpStatusCode.Accepted, orderId);
-                    break;
-                 
-                case "/addblock" when request.HttpMethod == "POST":
-                    try
-                    {
-                        var blockOrder = _orderController.PostAddBlock(request.QueryString, requestBody);
-                        SendResponse(context.Response, HttpStatusCode.Accepted, blockOrder);
-                    }
-                    catch (ArgumentException)
-                    {
-                        SendResponse(context.Response, HttpStatusCode.BadRequest);
-                    }
-                    break;
-                
-                case "/order" when request.HttpMethod == "GET":
-                    var order = _orderController.GetOrder(request.QueryString);
-                    if (order == null) SendResponse(context.Response, HttpStatusCode.NotFound);
-                    else SendResponse(context.Response, HttpStatusCode.Accepted, order);
-                    break;
-            
-                case "/orders" when request.HttpMethod == "GET":
-                    try
-                    {
-                        var orders = _orderController.GetAllOrders();
-                        SendResponse(context.Response, HttpStatusCode.Accepted, orders);
-                    }
-                    catch (ArgumentException)
-                    {
-                        SendResponse(context.Response, HttpStatusCode.BadRequest);
-                    }
-                    break;
-                
-                case "/order" when request.HttpMethod == "DELETE":
-                    var deleted = _orderController.Delete(request.QueryString);
-                    if (!deleted) SendResponse(context.Response, HttpStatusCode.BadRequest);
-                    else SendResponse(context.Response, HttpStatusCode.Accepted);
-                    break;
-                
-                case "/order" when request.HttpMethod == "PUT":
-                    var submittedOrder = _orderController.Put(request.QueryString);
-                    if (submittedOrder == null) SendResponse(context.Response, HttpStatusCode.BadRequest);
-                    else SendResponse(context.Response, HttpStatusCode.Accepted, submittedOrder); 
-                    break;
-                
-                case "/report" when request.HttpMethod == "GET":
-                    try
-                    {
-                        var report = _orderController.GetReport(request.QueryString);
-                        SendResponse(context.Response, HttpStatusCode.Accepted, report);
-                    }
-                    catch (InvalidDataException)
-                    {
-                        SendResponse(context.Response, HttpStatusCode.Forbidden);
-                    }
-                    break;
-            }*/
         }
-
-        private static void SendResponse(HttpListenerResponse response, HttpStatusCode statusCode, Object @object = null)
-        {
-            Console.WriteLine("End of client data.");
-            response.Headers.Set("Server", "mona's-server");
-            response.StatusCode = (int) statusCode;
-            if (@object == null) return;              //TODO: CAUSING THREADING ISSUES!!
-            
-            var options = new JsonSerializerOptions
-            {
-                Converters = {new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)}
-            };
-            string responseString = JsonSerializer.Serialize(@object, options);
-            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
-            response.ContentType = "application/json";
-            response.ContentLength64 = buffer.Length;
-            var output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            output.Close();
-            response.Close();
-        }
+        
         
     }
 }
